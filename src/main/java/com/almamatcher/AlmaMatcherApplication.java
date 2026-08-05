16be4e9135aff3d75d60fc24eae140556c9efa13
Daniele Zappala <danielezap11@gmail.com>
2026-08-05 03:05:53 +0200

    refactor: used initializr for spring project that made a project conf by itself

---
+++ src/main/java/com/almamatcher/AlmaMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.almamatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmaMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmaMatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almamatcher/AlmaMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almamatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmaMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
