1f5622fd2629602b7031471042a4e03a82771c71
Daniele Zappala <danielezap11@gmail.com>
2026-08-09 04:29:28 +0200

    add: a constructor that accept a username to create a message about the already taken username.

--- src/main/java/com/almamatcher/model/exceptions/UsernameAlreadyTakenException.java
+++ src/main/java/com/almamatcher/model/exceptions/UsernameAlreadyTakenException.java
@@ -2,4 +2,7 @@ package com.almamatcher.model.exceptions;
 
 public class UsernameAlreadyTakenException extends Exception {
     
+    public UsernameAlreadyTakenException(String username) {
+        super(username + " already exists. Try another one.");
+    }
 }
