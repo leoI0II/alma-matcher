b6c022ee96844b35ed4e5b0991a9f4d298379b78
Daniele Zappala <danielezap11@gmail.com>
2026-08-16 23:36:44 +0200

    add: the method to the jpa repo interface that find all the tokens. app now saves (if i correctly understood) the datas of the tables in docker even if the app is down. add new verified.html page now as a puppet page just for test. the frontend part is not determined.

--- src/main/java/com/almamatcher/repository/EmailVerificationTokenRepository.java
+++ src/main/java/com/almamatcher/repository/EmailVerificationTokenRepository.java
@@ -1,5 +1,6 @@
 package com.almamatcher.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,5 +12,6 @@ public interface EmailVerificationTokenRepository
         extends JpaRepository<EmailVerificationToken, UUID> {
 
     Optional<EmailVerificationToken> findByToken(String token);
-    
+
+    List<EmailVerificationToken> findByAccountIdAndUsedAtIsNull(UUID accountId);
 }
